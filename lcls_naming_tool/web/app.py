ba2b8a13401edd36748a1ecacddc7fc0960ad634
Jane Liu <janeliu@slac.stanford.edu>
2024-09-11 16:20:27 -0700

    Fixed issues with web app.

---
+++ lcls_naming_tool/web/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+from lcls_naming_tool import load_taxons, validate
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    if request.method == 'POST':
+        pv_name = str(request.form['pv_name'])
+
+        flash(pv_name)
+
+        load_taxons()
+
+        is_valid = validate(pv_name)
+
+        if is_valid:
+            flash('Valid')
+        else:
+            flash('Invalid')
+            
+            
+    # return redirect(url_for('index'))
+
+    return render_template('index.html')
