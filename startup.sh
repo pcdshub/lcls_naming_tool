e582875115ed2774b7d1dee1778198eb17dbe0be
Jane Liu <janeliu@slac.stanford.edu>
2024-09-19 16:19:05 -0700

    Updated startup.sh with new python venv path.

--- startup.sh
+++ startup.sh
@@ -3,7 +3,11 @@
 
 export ACCESS_LOG_FORMAT='%(h)s %(l)s %({REMOTE_USER}i)s %(t)s "%(r)s" "%(q)s" %(s)s %(b)s %(D)s'
 
-source /cds/home/j/janeliu/git/lcls_naming_tool/venv/bin/activate
+source /cds/group/pcds/pyps/conda/venvs/lcls_naming_tool/bin/activate
+
+python -m pip install --upgrade pip
+
+pip install gunicorn
 
 exec gunicorn start:app -b 0.0.0.0:8319 \
        --log-level=${LOG_LEVEL} --capture-output --enable-stdio-inheritance \
