bf8d40157c57ebd23f47bd750f9a963942de1978
Jane Liu <janeliu@slac.stanford.edu>
2024-09-19 11:16:04 -0700

    Updated startup.sh to source a pcds_conda-based custom venv.

--- startup.sh
+++ startup.sh
@@ -3,19 +3,7 @@
 
 export ACCESS_LOG_FORMAT='%(h)s %(l)s %({REMOTE_USER}i)s %(t)s "%(r)s" "%(q)s" %(s)s %(b)s %(D)s'
 
-source /reg/g/pcds/engineering_tools/latest-released/scripts/pcds_conda
-
-source /cds/home/j/janeliu/git/lcls_naming_tool/venv/bin/activate
-
-cd lcls_naming_tool/web
-
-rm lcls_naming_tool.py
-
-rm -rf taxons
-
-cp ../lcls_naming_tool.py .
-
-cp -r ../taxons .
+source /cds/home/j/janeliu/git/lcls_naming_tool/naming-tool-venv/bin/activate
 
 exec gunicorn start:app -b 0.0.0.0:8319 \
        --log-level=${LOG_LEVEL} --capture-output --enable-stdio-inheritance \
