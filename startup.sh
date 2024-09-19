a331de5839f1346f1867c68577891c30cfa3f358
Jane Liu <janeliu@slac.stanford.edu>
2024-09-18 17:19:12 -0700

    Removed commands for installation in startup.sh.

--- startup.sh
+++ startup.sh
@@ -3,13 +3,7 @@
 
 export ACCESS_LOG_FORMAT='%(h)s %(l)s %({REMOTE_USER}i)s %(t)s "%(r)s" "%(q)s" %(s)s %(b)s %(D)s'
 
-source pcds_conda
-
-pip3 install -r requirements.txt
-
-python3 -m venv venv
-
-source venv/bin/activate
+source pcds_conda  # install gunicorn in pcds_conda
 
 cd lcls_naming_tool/web
 
@@ -21,12 +15,7 @@ cp ../lcls_naming_tool.py .
 
 cp -r ../taxons .
 
-export FLASK_APP=app
-
-flask --app app run
-
-gunicorn --config gunicorn_config.py app:app
-
---log-level=${LOG_LEVEL} --capture-output --enable-stdio-inheritance \
---timeout 300 --graceful-timeout 1 \
---access-logfile - --access-logformat "${ACCESS_LOG_FORMAT}"
+exec gunicorn start:app -b 0.0.0.0:8319 \
+       --log-level=${LOG_LEVEL} --capture-output --enable-stdio-inheritance \
+       --timeout 300 --graceful-timeout 1 \
+       --access-logfile - --access-logformat "${ACCESS_LOG_FORMAT}"
