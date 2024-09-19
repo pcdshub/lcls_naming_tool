e0ab2d25c46b004f4d0d0189713116aae3302033
Jane Liu <janeliu@slac.stanford.edu>
2024-09-19 16:35:13 -0700

    Fixed error in previous commit (can't install on psctlws01).

--- startup.sh
+++ startup.sh
@@ -5,10 +5,6 @@ export ACCESS_LOG_FORMAT='%(h)s %(l)s %({REMOTE_USER}i)s %(t)s "%(r)s" "%(q)s" %
 
 source /cds/group/pcds/pyps/conda/venvs/lcls_naming_tool/bin/activate
 
-python -m pip install --upgrade pip
-
-pip install gunicorn
-
 exec gunicorn start:app -b 0.0.0.0:8319 \
        --log-level=${LOG_LEVEL} --capture-output --enable-stdio-inheritance \
        --timeout 300 --graceful-timeout 1 \
