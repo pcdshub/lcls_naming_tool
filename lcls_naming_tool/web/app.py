f7d0556e5fb574464dd4a4b9ae1286d1fde9354d
Jane Liu <janeliu@slac.stanford.edu>
2024-09-19 10:51:19 -0700

    Updated import directory for app.py.

--- lcls_naming_tool/web/app.py
+++ lcls_naming_tool/web/app.py
@@ -1,9 +1,14 @@
+import sys
+import os
 from flask import Flask, render_template, request, url_for, flash, redirect
 from lcls_naming_tool import get_version, load_taxons, validate
 
 
+sys.path.append(os.path.abspath('../'))
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
+
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
