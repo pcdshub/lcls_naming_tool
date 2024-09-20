9c51e16ee03bafab21d2ea2b3e489cd0fe7f0c2e
Jane Liu <janeliu@slac.stanford.edu>
2024-09-20 11:25:22 -0700

    Couldn't get absolute file path code working, so copying lcls_naming_tool.py and /taxons to /web for now. Will revisit to fix after website is running.

--- startup.sh
+++ startup.sh
@@ -5,6 +5,10 @@ export ACCESS_LOG_FORMAT='%(h)s %(l)s %({REMOTE_USER}i)s %(t)s "%(r)s" "%(q)s" %
 
 source /cds/group/pcds/pyps/conda/venvs/lcls_naming_tool/bin/activate
 
+cp ./lcls_naming_tool/lcls_naming_tool.py ./lcls_naming_tool/web
+
+cp -r ./lcls_naming_tool/taxons ./lcls_naming_tool/web
+
 cd ./lcls_naming_tool/web
 
 exec gunicorn app:app -b 0.0.0.0:8319 \
