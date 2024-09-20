49af7e7e41000a2219d18ec54f981723bea1511e
Jane Liu <janeliu@slac.stanford.edu>
2024-09-19 17:26:52 -0700

    In startup.sh added logic to cd to the /web folder.

--- startup.sh
+++ startup.sh
@@ -5,7 +5,9 @@ export ACCESS_LOG_FORMAT='%(h)s %(l)s %({REMOTE_USER}i)s %(t)s "%(r)s" "%(q)s" %
 
 source /cds/group/pcds/pyps/conda/venvs/lcls_naming_tool/bin/activate
 
-exec gunicorn start:app -b 0.0.0.0:8319 \
+cd ./lcls_naming_tool/web
+
+exec gunicorn app:app -b 0.0.0.0:8319 \
        --log-level=${LOG_LEVEL} --capture-output --enable-stdio-inheritance \
        --timeout 300 --graceful-timeout 1 \
        --access-logfile - --access-logformat "${ACCESS_LOG_FORMAT}"
