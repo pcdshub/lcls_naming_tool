72686a78b5a1736e6188d97f1e297d0529400fa5
Jane Liu <janeliu@slac.stanford.edu>
2024-10-08 13:27:31 -0700

    Moved version = get_version() into args.version.

--- lcls_naming_tool/lcls_naming_tool.py
+++ lcls_naming_tool/lcls_naming_tool.py
@@ -1,57 +1,54 @@
 #!/usr/bin/env python3
 
-'''
-    LCLS Naming Tool
+"""
+LCLS Naming Tool
 
-    A tool for checking the form and content (but not meaning) of names with
-    respect to the LCLS naming convention.
+A tool for checking the form and content (but not meaning) of names with
+respect to the LCLS naming convention.
 
-    Functional component source letters and beampath numbers come from
-    https://docs.google.com/drawings/d/1Sg7pT2TlKq8PgM7UstmJJBLKeqROQeDWbkD0sDoTDdY/edit
+Functional component source letters and beampath numbers come from
+https://docs.google.com/drawings/d/1Sg7pT2TlKq8PgM7UstmJJBLKeqROQeDWbkD0sDoTDdY/edit
 
-    A valid name is in the format FFFFF:GGG:CCC:NN:XXXX, where GGG and NN are
-    optional and can be omitted. The XXXX is omitted for device names.
+A valid name is in the format FFFFF:GGG:CCC:NN:XXXX, where GGG and NN are
+optional and can be omitted. The XXXX is omitted for device names.
 
-    All permutations of valid names according to
-    LCLS_Photon_Source_and_Systems_Nomenclature.pdf. Names ending in XXXX are
-    PV names. Without XXXX are device names.
+All permutations of valid names according to
+LCLS_Photon_Source_and_Systems_Nomenclature.pdf. Names ending in XXXX are
+PV names. Without XXXX are device names.
 
-        FFFFF:CCC
-        FFFFF:CCC:NN
-        FFFFF:CCC:XXXX
-        FFFFF:CCC:NN:XXXX
-        FFFFF:GGG:CCC
-        FFFFF:GGG:CCC:NN
-        FFFFF:GGG:CCC:XXXX
-        FFFFF:GGG:CCC:NN:XXXX
+    FFFFF:CCC
+    FFFFF:CCC:NN
+    FFFFF:CCC:XXXX
+    FFFFF:CCC:NN:XXXX
+    FFFFF:GGG:CCC
+    FFFFF:GGG:CCC:NN
+    FFFFF:GGG:CCC:XXXX
+    FFFFF:GGG:CCC:NN:XXXX
 
-    This script only checks the validity of PV and device names.
-'''
+This script only checks the validity of PV and device names.
+"""
 
 import argparse
+import json
+import os
+import re
 import subprocess
 import sys
-import os
-import json
-import re
 
-
-lcls_taxons_cfg = str(os.environ.get('LCLS_NAMING_TOOL_TAXONS_DIR'))
+lcls_taxons_cfg = str(os.environ.get("LCLS_NAMING_TOOL_TAXONS_DIR"))
 
 fc_dict = {}
 fg_dict = {}
 ccc_dict = {}
-beam_sources = ('K', 'L')
-beam_numbers = ('0', '1', '2', '3', '4', '5')
+beam_sources = ("K", "L")
+beam_numbers = ("0", "1", "2", "3", "4", "5")
 
 
 def get_version():
     result = subprocess.run(
-        ['git', 'describe', '--tags'],
-        capture_output=True,
-        text=True
+        ["git", "describe", "--tags"], capture_output=True, text=True
     )
-    return (str(result.stdout).strip().split('-'))[0]
+    return (str(result.stdout).strip().split("-"))[0]
 
 
 def load_taxons():
@@ -61,27 +58,26 @@ def load_taxons():
     global lcls_taxons_cfg
 
     # Read the json files containing all the taxons
-    with open(lcls_taxons_cfg + '/functional_component_taxon.json') as fc_file:
+    with open(lcls_taxons_cfg + "/functional_component_taxon.json") as fc_file:
         fc_dict = json.load(fc_file)
 
-    with open(lcls_taxons_cfg + '/fungible_element_taxon.json') as fg_file:
+    with open(lcls_taxons_cfg + "/fungible_element_taxon.json") as fg_file:
         fg_dict = json.load(fg_file)
 
-    with open(lcls_taxons_cfg + '/ccc_taxon.json') as ccc_file:
+    with open(lcls_taxons_cfg + "/ccc_taxon.json") as ccc_file:
         ccc_dict = json.load(ccc_file)
 
 
 def functional_component_is_valid(fc_taxon):
-
     fc_length = len(fc_taxon)
 
-    if (4 < len(fc_taxon) < 9):
+    if 4 < len(fc_taxon) < 9:
         fc_prefix = fc_taxon[:2]  # max length is currently 2 letters
-        fc_seq_num = fc_taxon[2:fc_length-2]  # max length set to 4
+        fc_seq_num = fc_taxon[2 : fc_length - 2]  # max length set to 4
         # max length is currently 1 letter
-        fc_source_ltr = fc_taxon[fc_length-2:fc_length-1]
+        fc_source_ltr = fc_taxon[fc_length - 2 : fc_length - 1]
         # max length is currently 1 digit
-        fc_beam_num = fc_taxon[fc_length-1:fc_length]
+        fc_beam_num = fc_taxon[fc_length - 1 : fc_length]
     else:
         return False
 
@@ -144,14 +140,14 @@ def increment_is_valid(nn_taxon):
 
 def starts_alphanumeric(taxon):
     # check the element starts with a letter or number
-    return bool(re.search('^[a-zA-Z0-9]', taxon))
+    return bool(re.search("^[a-zA-Z0-9]", taxon))
 
 
 def validate(user_input):
-    if ('.' in user_input) or (len(user_input) > 60):
+    if ("." in user_input) or (len(user_input) > 60):
         return False
 
-    name = [x.strip() for x in user_input.upper().split(':')]
+    name = [x.strip() for x in user_input.upper().split(":")]
 
     for element in name:
         if not starts_alphanumeric(element):
@@ -169,7 +165,7 @@ def validate(user_input):
 
     if fc_valid:
         # Check for name with 2 elements (FFFFF:CCC)
-        if (len(name) == 2):
+        if len(name) == 2:
             ccc_valid = constituent_component_is_valid(second_element)
 
             if ccc_valid:
@@ -180,7 +176,7 @@ def validate(user_input):
         third_element = name[2]
 
         # check for name with 3 elements (FFFFF:GGG:CCC, FFFFF:CCC:NN, FFFFF:CCC:XXXX)
-        if (len(name) == 3):
+        if len(name) == 3:
             fg_valid = fungible_is_valid(second_element)
             ccc_valid = constituent_component_is_valid(second_element)
 
@@ -202,7 +198,7 @@ def validate(user_input):
         fourth_element = name[3]
 
         # Check for name with 4 elements (FFFFF:GGG:CCC:NN, FFFFF:GGG:CCC:XXXX, FFFFF:CCC:NN:XXXX)
-        if (len(name) == 4):
+        if len(name) == 4:
             fg_valid = fungible_is_valid(second_element)
             ccc_valid = constituent_component_is_valid(second_element)
 
@@ -228,7 +224,7 @@ def validate(user_input):
                 return False
 
         # check for name with 5 elements (FFFFF:GGG:CCC:NN:XXXX)
-        if (len(name) == 5):
+        if len(name) == 5:
             fg_taxon = name[1]
             fg_valid = fungible_is_valid(fg_taxon)
 
@@ -251,22 +247,17 @@ def validate(user_input):
 
 
 def main():
-
-    version = get_version()
-
     load_taxons()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('string',
-                        nargs='*',
-                        type=str,
-                        help='PV or device name to validate')
-    parser.add_argument('-v',
-                        '--version',
-                        action='store_true')
+    parser.add_argument(
+        "string", nargs="*", type=str, help="PV or device name to validate"
+    )
+    parser.add_argument("-v", "--version", action="store_true")
     args = parser.parse_args()
 
     if args.version:
+        version = get_version()
         print(version)
         sys.exit()
 
@@ -277,9 +268,9 @@ def main():
     # Process the strings
     for string in args.string:
         if validate(string):
-            print('Valid')
+            print("Valid")
         else:
-            print('Invalid')
+            print("Invalid")
 
 
 if __name__ == "__main__":
