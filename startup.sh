9cb990f50fdfb8000ef877a860987b44e2976750
Jane Liu <janeliu@slac.stanford.edu>
2024-10-16 15:06:22 -0700

    Updated startup.sh with a more reliable way for bash script to get the full path to the lcls_naming_tool module directory (cannot pip install modules on psctlws01).

--- startup.sh
+++ startup.sh
@@ -5,11 +5,13 @@ export ACCESS_LOG_FORMAT='%(h)s %(l)s %({REMOTE_USER}i)s %(t)s "%(r)s" "%(q)s" %
 
 export LCLS_NAMING_TOOL_TAXONS_DIR="/cds/group/pcds/pyps/apps/lcls_naming_tool/taxons"
 
+export HOME_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+export PYTHONPATH="${HOME_DIR}/lcls_naming_tool"
+
 source /cds/group/pcds/pyps/conda/venvs/lcls_naming_tool/bin/activate
 
-cp ./lcls_naming_tool/lcls_naming_tool.py ./lcls_naming_tool/web
-
-cd ./lcls_naming_tool/web
+cd "${HOME_DIR}/lcls_naming_tool/web"
 
 exec gunicorn app:app -b 0.0.0.0:8319 \
        --log-level=${LOG_LEVEL} --capture-output --enable-stdio-inheritance \
